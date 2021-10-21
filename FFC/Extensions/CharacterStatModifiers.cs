4ad767a3263dbb8f796f291f45296b91e6dbe5b3
Keenan Johns <keenanjj13@gmail.com>
2021-10-21 12:19:28 -0500

    WIP - Scaling Health for Jugg

--- FFC/Extensions/CharacterStatModifiers.cs
+++ FFC/Extensions/CharacterStatModifiers.cs
@@ -1,5 +1,16 @@
 ﻿namespace FFC.Extensions {
     public class CharacterStatModifiers {
-        
+        public class CharacterStatModifiersAdditionalData {
+            public float MovementSpeedCeiling;
+
+            public CharacterStatModifiersAdditionalData() {
+                MovementSpeedCeiling = 0f;
+            }
+        }
+
+
+        public static class CharacterStatModifiersExtension {
+            
+        }
     }
 }
